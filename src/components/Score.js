4dd93585638a6f2503ef63456848ee226423f35b
Adam Mann <aemann2@gmail.com>
2021-05-07 13:02:23 -0700

    refactored score, began refactoring wordchanger

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,8 +1,38 @@
 import React, { useContext } from 'react';
 import { ScoreContext } from '../context/ScoreContext';
+import { WordsContext } from '../context/WordsContext';
+import { GameStateContext } from '../context/GameStateContext';
+import useKeypress from '../hooks/useKeypress';
 
 const Score = () => {
-  const { score } = useContext(ScoreContext);
+  const { score, setScore } = useContext(ScoreContext);
+  const { currentWord, substring } = useContext(WordsContext);
+  const { isGameOver, isTimeOut } = useContext(GameStateContext);
+
+  useKeypress((e) => {
+    if (currentWord && !isGameOver && !isTimeOut) {
+      // if a substring has been created (after a first character has been typed)
+      if (substring) {
+        // if it's the last character and the right letter...
+        if (e.key === substring[0] && substring.length === 1) {
+          setScore(score + 10);
+          // elif it's the right letter...
+        } else if (e.key === substring[0]) {
+          setScore(score + 10);
+          // if it's the wrong letter
+        } else {
+          setScore(score - 10);
+        }
+      } else if (e.key === currentWord[0]) {
+        // if it's the right character of the first letter, set the substring and highlight
+        setScore(score + 10);
+      } else {
+        //otherwise, switch the word out
+        setScore(score - 10);
+      }
+    }
+  });
+
   return (
     <>
       <h2>Score:{score} </h2>
--- src/components/WordChanger.js
+++ src/components/WordChanger.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useContext } from 'react';
 import { WordsContext } from '../context/WordsContext';
+import { GameStateContext } from '../context/GameStateContext';
 import * as Helper from '../utils/helpers';
 import fetchArray from '../utils/fetchArray';
 import useKeypress from '../hooks/useKeypress';
@@ -11,10 +12,12 @@ const WordChanger = () => {
     setWordArray,
     currentWord,
     setCurrentWord,
-    setSubstring,
     toHighlight,
     setToHighlight,
+    substring,
+    setSubstring,
   } = useContext(WordsContext);
+  const { isGameOver, isTimeOut } = useContext(GameStateContext);
 
   let regex = null;
 
@@ -36,11 +39,39 @@ const WordChanger = () => {
     setToHighlight('');
   };
 
-  useKeypress(changeCurrentWord);
-
   if (toHighlight) {
     regex = new RegExp(toHighlight);
   }
+
+  useKeypress((e) => {
+    if (currentWord && !isGameOver && !isTimeOut) {
+      // if a substring has been created (after a first character has been typed)
+      if (substring) {
+        // if it's the last character and the right letter...
+        if (e.key === substring[0] && substring.length === 1) {
+          resetSubstringAndHighlight();
+          setToHighlight(toHighlight + e.key);
+          changeCurrentWord();
+          // elif it's the right letter...
+        } else if (e.key === substring[0]) {
+          setSubstring(substring.slice(1, currentWord.length));
+          setToHighlight(toHighlight + e.key);
+          // if it's the wrong letter
+        } else {
+          resetSubstringAndHighlight();
+          changeCurrentWord();
+        }
+      } else if (e.key === currentWord[0]) {
+        // if it's the right character of the first letter, set the substring and highlight
+        setSubstring(currentWord.slice(1, currentWord.length));
+        setToHighlight(toHighlight + e.key);
+      } else {
+        //otherwise, switch the word out
+        resetSubstringAndHighlight();
+        changeCurrentWord();
+      }
+    }
+  });
 
   return (
     <main>
